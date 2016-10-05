d51fec5d66b3bf1297f29592133d976cb7553a00
Chris Jaffier <chrisd@Chriss-MacBook-Pro.local>
2016-10-05 15:52:49 -0400

    pokemon logic made

---
+++ db/app.rb
@@ -0,0 +1,10 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'active_record'
+require 'pg'
+require 'pry'
+
+require_relative 'db/connection'
+require_relative 'models/pokemon'
+
+binding.pry
